2790dc0891f53703c4fb59d10ebe29388063c1ec
emmiller-gaming <emily.isaacson@pcc.edu>
2026-08-03 17:50:33 -0700

    Met W Teacher Again
    Fixed movement and added collision boxes. Have it so text box appears when entering collision box.

--- PlayerCharacter/PlayerCharacter.gd
+++ PlayerCharacter/PlayerCharacter.gd
@@ -1,24 +1,27 @@
-extends Node3D
+extends CharacterBody3D
 class_name PlayerCharacter
 
-var move_distance = 1.0
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("move_right"):
-		position.x += move_distance
-		print("Pressed D")
-	
-	if Input.is_action_just_pressed("move_left"):
-		position.x -= move_distance
-		print("Pressed A")
-		
-	if Input.is_action_just_pressed("move_up"):
-		position.z -= move_distance # forward
-		print("Pressed W")
-		
-	if Input.is_action_just_pressed("move_down"):
-		position.z += move_distance # backward
-		print("Pressed S")
-	pass
-	
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	var input_dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
---
+++ Scenes/Rat.gd
@@ -0,0 +1,22 @@
+extends Area3D
+class_name Rat
+@onready var speech_bubble: PanelContainer = $SpeechBubble
+@onready var message: Label = $SpeechBubble/VBox/Message
+@onready var accept_button: Button = $SpeechBubble/VBox/AcceptButton
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	body_entered.connect(OnBodyEntered)
+	speech_bubble.hide()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	# if PlayerCharacter CollisionObject3D:
+		# print("Squeak!")
+		
+	
+	pass
+
+func OnBodyEntered(other_body):
+	speech_bubble.show()
